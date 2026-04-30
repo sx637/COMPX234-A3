844b99ffd452203c4423bdd6ef4d8ce16ba71eec
sx637 <3038664080@qq.com>
2026-04-30 23:05:22 +0800

    Update TupleSpaceServer_help.py
     PUT — add (key, value) only if key does not already exist.

--- TupleSpaceServer_help.py
+++ TupleSpaceServer_help.py
@@ -181,6 +181,22 @@ def handle_request(message):
             # Validate: len(value) <= 999 and len(key + " " + value) <= 970.
             # Return "OK (<key>, <value>) added" or "ERR <key> already exists".
             increment_stat("put_count")
+            if len(value) > 999:
+                increment_stat("error_count")
+                return "ERR Value too long"
+            
+            if len(key) + 1 + len(value) > 970:
+                increment_stat("error_count")
+                return "ERR Key+value too long"
+            
+            if key in tuple_space:
+                increment_stat("error_count")
+                return f"ERR {key} already exists"
+            
+            tuple_space[key] = value
+            return f"OK ({key}, {value}) added"
+
+
 
 
         else:
